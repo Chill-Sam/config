8aa42a31ca151af04e48d43ac743e0593a2c09ca
Jonathan Wahrenberg <jonathan@wahrenberg.com>
2025-01-27 21:44:39 +0000

    Removed excess starship command

--- start.sh
+++ start.sh
@@ -2,9 +2,6 @@
 
 cp -a config_files/terminal/. $HOME/.config
 
-starship preset tokyo-night -o ~/.config/starship.toml
-echo "Starship setup"
-
 cp -a home/. $HOME/
 rm $HOME/.bash* 
 exec /bin/zsh
