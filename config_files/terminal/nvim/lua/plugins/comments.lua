bb010b85546bc9121709a407831874c836c4266d
Chill-Sam <jonathan@wahrenberg.com>
2025-01-28 09:34:34 +0100

    Added some plugins and added upload for neovim

--- config_files/terminal/nvim/lua/plugins/comments.lua
+++ config_files/terminal/nvim/lua/plugins/comments.lua
@@ -2,6 +2,21 @@ return {
     {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {}
-    }
+        opts = {},
+    },
+
+    { -- Adds git related signs to the gutter, as well as utilities for managing changes
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            signs = {
+                add = { text = "+" },
+                change = { text = "~" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+            },
+        },
+    },
+
+    "tpope/vim-sleuth", -- Tab detection
 }
